abde92c48916738c4e90cc488e360723ed921dc8
Ondřej Plátek <ondrej.platek@seznam.cz>
2022-11-04 15:07:01 +0100

    saving no pca and black (#1)
    * saving no pca and black
    
    * better logging

--- plda/model.py
+++ plda/model.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+import logging
+from typing import Optional, Union
 import numpy as np
 from sklearn.decomposition import PCA
 from scipy.stats import multivariate_normal as gaussian
@@ -20,12 +22,12 @@ from .optimizer import (
     get_posterior_params,
     get_posterior_predictive_params,
     optimize_maximum_likelihood,  # I.e. empirical Bayes.
-    calc_scatter_matrices
+    calc_scatter_matrices,
 )
 
 
 def get_space_walk(from_space, to_space):
-    U_model_to_D = ['U_model', 'U', 'X', 'D']
+    U_model_to_D = ["U_model", "U", "X", "D"]
     D_to_U_model = U_model_to_D[::-1]
 
     assert from_space in U_model_to_D and to_space in U_model_to_D
@@ -42,8 +44,8 @@ def get_space_walk(from_space, to_space):
     else:
         spaces = U_model_to_D
 
-    from_spaces = [x for x in spaces[from_idx: to_idx]]
-    to_spaces = [x for x in spaces[from_idx + 1: to_idx + 1]]
+    from_spaces = [x for x in spaces[from_idx:to_idx]]
+    to_spaces = [x for x in spaces[from_idx + 1 : to_idx + 1]]
 
     return zip(from_spaces, to_spaces)
 
@@ -78,9 +80,7 @@ def transform_X_to_D(data, pca):
 
 
 class Model:
-    def __init__(self, row_wise_data, labels, n_principal_components=None):
-        assert len(row_wise_data.shape) == 2
-        assert len(labels) == row_wise_data.shape[0]
+    def __init__(self):
 
         self.pca = None
         self.m = None
@@ -93,43 +93,41 @@ class Model:
         self.posterior_params = None
         self.posterior_predictive_params = None
 
-        self.fit(row_wise_data, labels, n_principal_components)
-
     def calc_logp_posterior(self, v_model, category):
-        assert v_model.shape[-1] == self.get_dimensionality('U_model')
+        assert v_model.shape[-1] == self.get_dimensionality("U_model")
 
-        mean = self.posterior_params[category]['mean']
-        cov_diag = self.posterior_params[category]['cov_diag']
+        mean = self.posterior_params[category]["mean"]
+        cov_diag = self.posterior_params[category]["cov_diag"]
 
         return gaussian(mean, np.diag(cov_diag)).logpdf(v_model)
 
     def calc_logp_posterior_predictive(self, U_model, category):
-        assert U_model.shape[-1] == self.get_dimensionality('U_model')
+        assert U_model.shape[-1] == self.get_dimensionality("U_model")
 
-        mean = self.posterior_predictive_params[category]['mean']
-        cov_diag = self.posterior_predictive_params[category]['cov_diag']
+        mean = self.posterior_predictive_params[category]["mean"]
+        cov_diag = self.posterior_predictive_params[category]["cov_diag"]
 
         return gaussian(mean, np.diag(cov_diag)).logpdf(U_model)
 
     def calc_logp_marginal_likelihood(self, U_model):
-        """ Computes the log marginal likelihood on axis=-2. """
-        assert U_model.shape[-1] == self.get_dimensionality('U_model')
+        """Computes the log marginal likelihood on axis=-2."""
+        assert U_model.shape[-1] == self.get_dimensionality("U_model")
 
         if len(U_model.shape) == 1:
             U_model = U_model[None, :]
 
         n = U_model.shape[-2]
-        psi_diag = self.prior_params['cov_diag']
+        psi_diag = self.prior_params["cov_diag"]
         n_psi_plus_eye = n * psi_diag + 1
 
-        log_constant = -.5 * n * np.log(2 * np.pi)
-        log_constant += -.5 * np.log(n_psi_plus_eye)
+        log_constant = -0.5 * n * np.log(2 * np.pi)
+        log_constant += -0.5 * np.log(n_psi_plus_eye)
 
-        sum_of_squares = np.sum(U_model ** 2, axis=-2)
-        log_exponent_1 = -.5 * sum_of_squares
+        sum_of_squares = np.sum(U_model**2, axis=-2)
+        log_exponent_1 = -0.5 * sum_of_squares
 
         mean = U_model.mean(axis=-2)
-        log_exponent_2 = .5 * (n ** 2 * psi_diag * mean ** 2)
+        log_exponent_2 = 0.5 * (n**2 * psi_diag * mean**2)
         log_exponent_2 /= n_psi_plus_eye
 
         logp_ml = log_constant + log_exponent_1 + log_exponent_2
@@ -138,21 +136,23 @@ class Model:
         return logp_ml
 
     def calc_logp_prior(self, v_model):
-        assert v_model.shape[-1] == self.get_dimensionality('U_model')
+        assert v_model.shape[-1] == self.get_dimensionality("U_model")
 
-        mean = self.prior_params['mean']
-        cov_diag = self.prior_params['cov_diag']
+        mean = self.prior_params["mean"]
+        cov_diag = self.prior_params["cov_diag"]
 
         return gaussian(mean, np.diag(cov_diag)).logpdf(v_model)
 
     def calc_same_diff_log_likelihood_ratio(self, U_model_p, U_model_g):
-        assert U_model_p.shape[-1] == self.get_dimensionality('U_model')
-        assert U_model_g.shape[-1] == self.get_dimensionality('U_model')
+        assert U_model_p.shape[-1] == self.get_dimensionality("U_model")
+        assert U_model_g.shape[-1] == self.get_dimensionality("U_model")
 
-        U_model_same = np.concatenate([
-            U_model_p,
-            U_model_g,
-        ])
+        U_model_same = np.concatenate(
+            [
+                U_model_p,
+                U_model_g,
+            ]
+        )
         ll_same = self.calc_logp_marginal_likelihood(U_model_same)
         ll_p = self.calc_logp_marginal_likelihood(U_model_p)
         ll_g = self.calc_logp_marginal_likelihood(U_model_g)
@@ -160,45 +160,98 @@ class Model:
 
         return ll_same
 
-    def fit(self, data, labels, n_principal_components=None):
-        if n_principal_components is None:
+    def fit(
+        self,
+        data,
+        labels,
+        feat_dim: Optional[int] = None,
+        pca: Optional[Union[PCA, str]] = None,
+    ):
+        assert len(data.shape) == 2
+        assert len(labels) == data.shape[0]
+
+        data_dim = data.shape[-1]
+
+        if pca is None:
+            logging.info("Estimating new PCA vector base")
             S_b, S_w = calc_scatter_matrices(data, labels)
             matrix_rank = np.linalg.matrix_rank(S_w)
-
-        else:
-            matrix_rank = n_principal_components
-
-        if matrix_rank != data.shape[-1]:
+            if feat_dim != matrix_rank:
+                logging.warning(
+                    f"The feat_dim estimated from data is {matrix_rank} "
+                    f"but you provide {feat_dim=} which will be used"
+                )
+                matrix_rank = feat_dim
             self.pca = PCA(n_components=matrix_rank)
+        elif pca == "skip":
+            self.pca = None
+        else:
+            logging.info(f"Replacing original pca {self.pca} with new pca {pca}")
+            self.pca = pca
+
+            if feat_dim is not None and pca.n_components_ != feat_dim:
+                raise ValueError(
+                    f"You requrested to use incompatible PCA {pca.n_components_} vs {feat_dim=}"
+                )
+            if data_dim != pca.n_features_:
+                raise ValueError(
+                    f"You provided incompatible PCA for the data {data_dim=} vs {pca.n_features_}"
+                )
+
+        if feat_dim is not None and feat_dim != data_dim:
+            if self.pca is None:
+                raise ValueError(
+                    f"{feat_dim=} is requested but {data_dim=} "
+                    f"but the data cannot be projected to {feat_dim=} without PCA"
+                )
+            else:
+                # See the other checks for supplied PCA when pca is assigned to self.pca
+                pass
+
+        if self.pca is None:
+            logging.warning(
+                f"Skipping PCA projection and decorelation. The data dimension wil be {data_dim}"
+            )
+        else:
             self.pca.fit(data)
+            if self.pca.n_components_ == data_dim:
+                logging.info(f"PCA keeps the {data_dim=} but decoralates the features")
+            else:
+                logging.info(
+                    f"PCA reduces {data_dim=} to {self.pca.n_components_} and decoralates the features"
+                )
 
-        X = self.transform(data, from_space='D', to_space='X')
+        X = self.transform(data, from_space="D", to_space="X")
 
-        self.m, self.A, self.Psi, self.relevant_U_dims, self.inv_A = \
-            optimize_maximum_likelihood(X, labels)
+        (
+            self.m,
+            self.A,
+            self.Psi,
+            self.relevant_U_dims,
+            self.inv_A,
+        ) = optimize_maximum_likelihood(X, labels)
 
-        U_model = self.transform(X, from_space='X', to_space='U_model')
+        U_model = self.transform(X, from_space="X", to_space="U_model")
 
-        self.prior_params = \
-            get_prior_params(self.Psi, self.relevant_U_dims)
+        self.prior_params = get_prior_params(self.Psi, self.relevant_U_dims)
 
-        self.posterior_params = \
-            get_posterior_params(U_model, labels, self.prior_params)
+        self.posterior_params = get_posterior_params(U_model, labels, self.prior_params)
 
-        self.posterior_predictive_params = \
-            get_posterior_predictive_params(self.posterior_params)
+        self.posterior_predictive_params = get_posterior_predictive_params(
+            self.posterior_params
+        )
 
     def get_dimensionality(self, space):
-        if space == 'U_model':
+        if space == "U_model":
             return self.relevant_U_dims.shape[0]
 
-        elif space == 'U':
+        elif space == "U":
             return self.A.shape[0]
 
-        elif space == 'X':
+        elif space == "X":
             return self.A.shape[0]
 
-        elif space == 'D':
+        elif space == "D":
             if self.pca is None:
                 return self.m.shape[0]
 
@@ -209,7 +262,7 @@ class Model:
             raise ValueError
 
     def transform(self, data, from_space, to_space):
-        """ Potential_spaces: 'D' <---> 'X' <---> 'U' <---> 'U_model'.
+        """Potential_spaces: 'D' <---> 'X' <---> 'U' <---> 'U_model'.
 
         DESCRIPTION
          There are 6 basic transformations to move back and forth
@@ -241,24 +294,24 @@ class Model:
         if len(data.shape) == 1:
             data = data[None, :]
 
-        if from_space == 'D' and to_space == 'X':
+        if from_space == "D" and to_space == "X":
             return transform_D_to_X(data, self.pca)
 
-        elif from_space == 'X' and to_space == 'U':
+        elif from_space == "X" and to_space == "U":
             return transform_X_to_U(data, self.inv_A, self.m)
 
-        elif from_space == 'U' and to_space == 'U_model':
+        elif from_space == "U" and to_space == "U_model":
             return transform_U_to_U_model(data, self.relevant_U_dims)
 
-        elif from_space == 'U_model' and to_space == 'U':
-            dim = self.get_dimensionality('U')
+        elif from_space == "U_model" and to_space == "U":
+            dim = self.get_dimensionality("U")
 
             return transform_U_model_to_U(data, self.relevant_U_dims, dim)
 
-        elif from_space == 'U' and to_space == 'X':
+        elif from_space == "U" and to_space == "X":
             return transform_U_to_X(data, self.A, self.m)
 
-        elif from_space == 'X' and to_space == 'D':
+        elif from_space == "X" and to_space == "D":
             return transform_X_to_D(data, self.pca)
 
         else:
